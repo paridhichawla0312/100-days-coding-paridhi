ddbde8de7850f346dd25d7046088a18cc5517735
paridhichawla0312 <paridhi.chawla0312@gmail.com>
2026-09-12 23:42:44 +0530

    Add files via upload

---
+++ day34q67.c
@@ -0,0 +1,34 @@
+//Insert an element in an array at a given position.
+#include <stdio.h>
+
+int main() {
+    int a[100], n, i, element, position;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the array elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Enter the element to insert: ");
+    scanf("%d", &element);
+
+    printf("Enter the position: ");
+    scanf("%d", &position);
+
+    for (i = n; i >= position; i--) {
+        a[i] = a[i - 1];
+    }
+
+    a[position - 1] = element;
+    n++;
+
+    printf("Array after insertion:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
---
+++ day34q68.c
@@ -0,0 +1,30 @@
+//Delete an element from an array.
+#include <stdio.h>
+
+int main() {
+    int a[100], n, i, position;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the array elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Enter the position to delete: ");
+    scanf("%d", &position);
+
+    for (i = position - 1; i < n - 1; i++) {
+        a[i] = a[i + 1];
+    }
+
+    n--;
+
+    printf("Array after deletion:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
