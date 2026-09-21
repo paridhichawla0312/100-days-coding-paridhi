a7217c513d664fa39859da446aa710d41db99869
paridhichawla0312 <paridhi.chawla0312@gmail.com>
2026-09-22 00:01:05 +0530

    Add files via upload

---
+++ day43q86.c
@@ -0,0 +1,29 @@
+//Check if a string is a palindrome.
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int i, length, flag = 1;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    length = strlen(str);
+
+    for (i = 0; i < length / 2; i++) {
+        if (str[i] != str[length - 1 - i]) {
+            flag = 0;
+            break;
+        }
+    }
+
+    if (flag == 1) {
+        printf("Palindrome");
+    }
+    else {
+        printf("Not palindrome");
+    }
+
+    return 0;
+}
